bc3592ba45d18dc1d8738b737ae45f895a9f290d
Raymond van Hoorn <vanhoornraymond@gmail.com>
2022-01-28 12:37:31 +0100

    Added REST API Endpoints and Unit Of Work pattern

---
+++ CarRentalManagement/Server/Controllers/ColoursController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using Microsoft.AspNetCore.Authorization;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ColoursController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ColoursController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Colours
+        [HttpGet]
+        public async Task<IActionResult> GetColours()
+        {
+            var colours = await _unitOfWork.Colours.GetAll();
+            return Ok(colours);
+        }
+
+        // GET: /Colours/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetColour(int id)
+        {
+            var colour = await _unitOfWork.Colours.Get(q => q.Id == id);
+
+            if (colour == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(colour);
+        }
+
+        // PUT: /Colours/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutColour(int id, Colour colour)
+        {
+            if (id != colour.Id)
+            {
+                return BadRequest();
+            }
+
+            _unitOfWork.Colours.Update(colour);
+
+            try
+            {
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)            // concurrency violation -> rij updaten die niet meer aanwezig is in database
+            {
+                if (!await ColourExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: /Colours
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Colour>> PostColour(Colour colour)
+        {
+            await _unitOfWork.Colours.Insert(colour);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetColour", new { id = colour.Id }, colour);
+        }
+
+        // DELETE: /Colours/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteColour(int id)
+        {
+            var colour = await _unitOfWork.Colours.Get(q => q.Id == id);
+            if (colour == null)
+            {
+                return NotFound();
+            }
+            await _unitOfWork.Colours.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        private async Task<bool> ColourExists(int id)
+        {
+            var colour = await _unitOfWork.Colours.Get(q => q.Id == id);
+            return colour != null;
+        }
+    }
+}
---
+++ CarRentalManagement/Server/Controllers/ModelsController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using Microsoft.AspNetCore.Authorization;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ModelsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ModelsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Models
+        [HttpGet]
+        public async Task<IActionResult> GetModels()
+        {
+            var models = await _unitOfWork.Models.GetAll();
+            return Ok(models);
+        }
+
+        // GET: /Models/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetModel(int id)
+        {
+            var model = await _unitOfWork.Models.Get(q => q.Id == id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+        // PUT: /Models/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutModel(int id, Model model)
+        {
+            if (id != model.Id)
+            {
+                return BadRequest();
+            }
+
+            _unitOfWork.Models.Update(model);
+
+            try
+            {
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)            // concurrency violation -> rij updaten die niet meer aanwezig is in database
+            {
+                if (!await ModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: /Models
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Model>> PostModel(Model model)
+        {
+            await _unitOfWork.Models.Insert(model);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetModel", new { id = model.Id }, model);
+        }
+
+        // DELETE: /Models/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteModel(int id)
+        {
+            var model = await _unitOfWork.Models.Get(q => q.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            await _unitOfWork.Models.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        private async Task<bool> ModelExists(int id)
+        {
+            var model = await _unitOfWork.Models.Get(q => q.Id == id);
+            return model != null;
+        }
+    }
+}
